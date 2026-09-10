15d6c4e2656e98ace643365560e7ca28fdf318a4
tobianahillel-afk <269188945+tobianahillel-afk@users.noreply.github.com>
2026-09-10 20:39:29 +0200

    feat: add private media Storage application port

---
+++ src/application/documents/private-media-storage-port.ts
@@ -0,0 +1,23 @@
+export interface UploadReservedMediaObjectInput {
+  readonly path: string;
+  readonly bytes: Uint8Array;
+  readonly mimeType: string;
+}
+
+export interface UploadReservedMediaObjectReceipt {
+  readonly bucket: "project-private";
+  readonly path: string;
+}
+
+export interface DeleteReservedMediaObjectReceipt {
+  readonly bucket: "project-private";
+  readonly path: string;
+  readonly absent: true;
+}
+
+export interface PrivateMediaStoragePort {
+  uploadReservedObject(
+    input: UploadReservedMediaObjectInput,
+  ): Promise<UploadReservedMediaObjectReceipt>;
+  deleteReservedObject(path: string): Promise<DeleteReservedMediaObjectReceipt>;
+}
